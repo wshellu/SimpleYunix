c3b8dd329e55dee7bca01354cc9e081754a93db4
wshellu <wshellu@github.com>
2015-05-14 23:03:35 -0400

    add busybox source code

---
+++ opensrc/busybox-1.22.1/include/applet_tables.h
@@ -0,0 +1,228 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 71
+
+const char applet_names[] ALIGN1 = ""
+"ash" "\0"
+"awk" "\0"
+"base64" "\0"
+"basename" "\0"
+"bash" "\0"
+"bzip2" "\0"
+"cat" "\0"
+"clear" "\0"
+"cp" "\0"
+"date" "\0"
+"dd" "\0"
+"df" "\0"
+"diff" "\0"
+"dirname" "\0"
+"egrep" "\0"
+"env" "\0"
+"expand" "\0"
+"expr" "\0"
+"false" "\0"
+"fgrep" "\0"
+"find" "\0"
+"free" "\0"
+"grep" "\0"
+"gzip" "\0"
+"halt" "\0"
+"head" "\0"
+"id" "\0"
+"ifconfig" "\0"
+"init" "\0"
+"kill" "\0"
+"killall" "\0"
+"killall5" "\0"
+"linuxrc" "\0"
+"ln" "\0"
+"logname" "\0"
+"ls" "\0"
+"lsof" "\0"
+"more" "\0"
+"mount" "\0"
+"mv" "\0"
+"ping" "\0"
+"poweroff" "\0"
+"ps" "\0"
+"pstree" "\0"
+"pwd" "\0"
+"reboot" "\0"
+"rm" "\0"
+"rmdir" "\0"
+"sed" "\0"
+"seq" "\0"
+"sh" "\0"
+"sleep" "\0"
+"sort" "\0"
+"tail" "\0"
+"tar" "\0"
+"time" "\0"
+"top" "\0"
+"touch" "\0"
+"tty" "\0"
+"umount" "\0"
+"uname" "\0"
+"unxz" "\0"
+"unzip" "\0"
+"uudecode" "\0"
+"vi" "\0"
+"wc" "\0"
+"whoami" "\0"
+"xargs" "\0"
+"xz" "\0"
+"xzcat" "\0"
+"yes" "\0"
+;
+
+#ifndef SKIP_applet_main
+int (*const applet_main[])(int argc, char **argv) = {
+ash_main,
+awk_main,
+base64_main,
+basename_main,
+ash_main,
+bzip2_main,
+cat_main,
+clear_main,
+cp_main,
+date_main,
+dd_main,
+df_main,
+diff_main,
+dirname_main,
+grep_main,
+env_main,
+expand_main,
+expr_main,
+false_main,
+grep_main,
+find_main,
+free_main,
+grep_main,
+gzip_main,
+halt_main,
+head_main,
+id_main,
+ifconfig_main,
+init_main,
+kill_main,
+kill_main,
+kill_main,
+init_main,
+ln_main,
+logname_main,
+ls_main,
+lsof_main,
+more_main,
+mount_main,
+mv_main,
+ping_main,
+halt_main,
+ps_main,
+pstree_main,
+pwd_main,
+halt_main,
+rm_main,
+rmdir_main,
+sed_main,
+seq_main,
+ash_main,
+sleep_main,
+sort_main,
+tail_main,
+tar_main,
+time_main,
+top_main,
+touch_main,
+tty_main,
+umount_main,
+uname_main,
+unxz_main,
+unzip_main,
+uudecode_main,
+vi_main,
+wc_main,
+whoami_main,
+xargs_main,
+unxz_main,
+unxz_main,
+yes_main,
+};
+#endif
+
+const uint16_t applet_nameofs[] ALIGN2 = {
+0x0000,
+0x0004,
+0x0008,
+0x000f,
+0x0018,
+0x001d,
+0x0023,
+0x0027,
+0x002d,
+0x0030,
+0x0035,
+0x0038,
+0x003b,
+0x0040,
+0x0048,
+0x004e,
+0x0052,
+0x0059,
+0x005e,
+0x0064,
+0x006a,
+0x006f,
+0x0074,
+0x0079,
+0x007e,
+0x0083,
+0x0088,
+0x008b,
+0x0094,
+0x0099,
+0x009e,
+0x00a6,
+0x00af,
+0x00b7,
+0x00ba,
+0x00c2,
+0x00c5,
+0x00ca,
+0x00cf,
+0x00d5,
+0x40d8,
+0x00dd,
+0x00e6,
+0x00e9,
+0x00f0,
+0x00f4,
+0x00fb,
+0x00fe,
+0x0104,
+0x0108,
+0x010c,
+0x010f,
+0x0115,
+0x011a,
+0x011f,
+0x0123,
+0x0128,
+0x012c,
+0x0132,
+0x0136,
+0x013d,
+0x0143,
+0x0148,
+0x014e,
+0x0157,
+0x015a,
+0x015d,
+0x0164,
+0x016a,
+0x016d,
+0x0173,
+};
+
